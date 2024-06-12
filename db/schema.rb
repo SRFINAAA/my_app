09c61091cded0d0b884e55788ef62fc9ff405dac
SRFINAAA <90295133+SRFINAAA@users.noreply.github.com>
2024-06-12 09:04:24 +0000

    selesai membuat halaman users, artikel, dan comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_01_085844) do
+ActiveRecord::Schema.define(version: 2024_06_12_085948) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -24,7 +24,8 @@ ActiveRecord::Schema.define(version: 2024_06_01_085844) do
   create_table "comments", force: :cascade do |t|
     t.integer "article_id", null: false
     t.string "comment"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
@@ -32,6 +33,8 @@ ActiveRecord::Schema.define(version: 2024_06_01_085844) do
     t.string "full_name"
     t.string "email"
     t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "articles", "users"
